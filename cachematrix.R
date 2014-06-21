27895f2470a1dd9f20b7cea99beb73aa3fbc5aa1
Tyler Field <tylerrfield@gmail.com>
2014-06-21 11:16:21 -0600

    skeleton of makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function contining a list of functions to set and get matrices and their inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invX <- NULL
+  set <- function(y) {
+    x <<- y
+    invX <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) invX <<- inverse
+  getInverse <- function() invX
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
